242179ee8a5b87f85575a636f4a1c284c48fd047
Devin Mullin <devin.mullin90@gmail.com>
2021-10-13 11:54:34 -0500

    commit tree 605cbb7741389fdd3fc010af5ce4cf1cf51bebe6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name = "Arel")
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+    badge_array = names.each do |str| 
+        puts ["Hello, my name is #{str}"]
+    end
+   
+end
+
+batch_badge_creator
